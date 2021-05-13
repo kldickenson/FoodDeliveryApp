cf93ce33b462098cbe90ccc68d7fa9da3103ea2a
Marie Antons <chefantons@gmail.com>
2021-05-13 17:59:28 -0500

    Add Cart Model-Migration

---
+++ VCFoodDeliveryApp/Migrations/20210513225324_Add Cart for user.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VCFoodDeliveryApp.Migrations
+{
+    public partial class AddCartforuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cart_Id",
+                schema: "Identity",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Cart",
+                schema: "Identity",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    ApplicationUser_Id = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    DeliveryTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OrderTotal = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cart", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cart_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalSchema: "Identity",
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cart_ApplicationUserId",
+                schema: "Identity",
+                table: "Cart",
+                column: "ApplicationUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cart",
+                schema: "Identity");
+
+            migrationBuilder.DropColumn(
+                name: "Cart_Id",
+                schema: "Identity",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ VCFoodDeliveryApp/Models/Cart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VCFoodDeliveryApp.Models
+{
+    public class Cart
+    {
+        [StringLength(450, ErrorMessage = "Too many users created")]
+        public string Id { get; set; }
+
+
+        public string ApplicationUser_Id { get; set; }
+
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public DateTime DeliveryTime { get; set; }
+
+
+        [DataType(DataType.Currency)]
+        public double OrderTotal { get; set; }
+
+    }
+}
